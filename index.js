b4bc10d2deb005813462febedad27671f6c7e8cc
Fredrick <freddyfred976@gmail.com>
2023-06-22 14:20:54 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(){
+    100 + 10
+}
+function subtract(){
+    60 - 10
+}
+function multiply(){
+    5 * 40
+}
+function divide (){
+    25 / 5
+}
+function add(a,b){
+    return (a+b)
+}
+function subtract(a,b){
+    return(a-b)
+}
+function multiply(a,b){
+    return(a*b)
+}
+function divide(a,b){
+    return(a/b)
+}
+function increment(n){
+    return (++n)
+}
+function decrement(n){
+    return(--n)
+}
+
+ const makeInt=(n)=>{
+    return parseInt(n, 10)
+ }
+
+ const preserveDecimal=(n)=>parseFloat(n)
